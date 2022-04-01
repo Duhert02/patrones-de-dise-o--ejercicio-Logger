cfbf0ec14ba8ad1918f460f156cc52ff81445e4c
Duhert02 <81563377+Duhert02@users.noreply.github.com>
2022-04-01 11:44:39 +0200

    Add files via upload

---
+++ pets/src/main/java/com/kreitek/pets/domain/Logger.java
@@ -0,0 +1,17 @@
+package com.kreitek.pets.domain;
+
+public class Logger {
+    public static void debug(String Aviso) {
+
+        int contador = 0 ;
+
+        System.out.println("[debug]" + "[" + contador + "]" + Aviso );
+    }
+
+}
+class TestLogger{
+    public static void main(String[] args) {
+
+
+    }
+}
